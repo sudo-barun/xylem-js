34bdafecb41c3f3efc4b018d0ec4d598aba5941b
Barun <kharelbarun@gmail.com>
2023-03-09 17:55:45 +0545

    cleanup

--- js/utilities/streamifiedSetTimeout.js
+++
@@ -1,8 +0,0 @@
-import createStream from "../core/createStream.js";
-export default function streamifiedSetTimeout(delay) {
-    const stream = createStream();
-    const intervalID = globalThis.setTimeout(stream, delay);
-    const clear = () => clearInterval(intervalID);
-    stream.clear = clear;
-    return stream;
-}
