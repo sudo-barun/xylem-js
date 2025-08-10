a4a7494fec1f5e137b0a3b38d958fb02a52515b3
Barun <kharelbarun@gmail.com>
2025-08-10 10:33:19 +0545

    mountComponent now accepts position

--- dom/mountComponent.js
+++ dom/mountComponent.js
@@ -1,9 +1,30 @@
-export default function mountComponent(component, element) {
+export default function mountComponent(component, node, position = 'beforeend') {
     component.setup();
     component.notifyAfterSetup();
     component.setupDom();
-    for (const element_ of component.domNodes()) {
-        element.appendChild(element_);
+    if (position === 'beforeend') {
+        for (const element_ of component.domNodes()) {
+            node.appendChild(element_);
+        }
+    }
+    else if (position === 'afterbegin') {
+        const firstChild = node.firstChild;
+        for (const element_ of component.domNodes()) {
+            node.insertBefore(element_, firstChild);
+        }
+    }
+    else if (position === 'afterend') {
+        let lastInsertedElement = node;
+        const componentNodes = component.domNodes();
+        for (let i = 0; i < componentNodes.length; i++) {
+            node.parentNode.insertBefore(componentNodes[i], lastInsertedElement.nextSibling);
+            lastInsertedElement = lastInsertedElement.nextSibling;
+        }
+    }
+    else if (position === 'beforebegin') {
+        for (const element_ of component.domNodes()) {
+            node.parentNode.insertBefore(element_, node);
+        }
     }
     component.notifyAfterAttachToDom();
     return () => {
